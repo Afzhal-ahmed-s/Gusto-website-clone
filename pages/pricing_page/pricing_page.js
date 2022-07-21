e2e2961c4c087a8dcf245e33b2b4d9c3f1a48075
Afzhal Ahmed S <writetoafzhalahmeds@gmail.com>
2022-07-21 14:24:40 +0530

    Co-authored-by: Ameer Khan <AmeerKhan1001@users.noreply.github.com>

--- pages/pricing_page/pricing_page.js
+++ pages/pricing_page/pricing_page.js
@@ -1,1 +1,13 @@
- 
+document.getElementById('i4_flex_right_container').addEventListener('click', function() {
+    let element = document.getElementsByClassName("i4v2_flex_container")[0];
+    element.classList.toggle("show-inner");
+    // if(element.classList.contains("show-inner")) element.classList.remove("show-inner");
+    // else element.classList.add("show-inner");
+    element = document.getElementById("i4_flex_right_container")
+    if(element.children[0].textContent == 'See plan details') {
+        element.children[0].textContent = 'Hide plan details';
+    }
+    else {
+        element.children[0].textContent = 'See plan details';
+    }
+});
