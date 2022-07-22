063801f7ec92e4373087c7e0e23463f421af5878
vijayhandal <handalvijay9588@gmail.com>
2022-07-22 07:52:39 +0530

    signup completed

---
+++ pages/Login_signup/scripts/signup.js
@@ -0,0 +1,13 @@
+
+
+function getStart(){
+    const signup_data={
+        name:document.getElementById('name').value,
+        email:document.getElementById('email').value,
+        c_name:document.getElementById('companyName').value,
+        no_of_emp:document.getElementById('no_of_emp').value,
+        com_phone:document.getElementById('com_phone').value,
+        password:document.getElementById('password').value,
+        Confirm_pass:document.getElementById('Confirm').value,
+    };
+}
