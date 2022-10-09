62ce7bdb8655c1d1d2185fe00d6002fe52b8d521
vijayhandal <handalvijay9588@gmail.com>
2022-10-09 17:28:38 +0530

    update

--- pricing_page.js
+++ pricing_page.js
@@ -1,7 +1,7 @@
-import Navbar from "/component/navbar.js";
+import Navbar from "../component/navbar.js";
     document.getElementById("Navbar").innerHTML =Navbar();
 
-    import footer from "/component/footer.js";
+    import footer from "../component/footer.js";
     document.getElementById("Footer").innerHTML =footer();
 
 
